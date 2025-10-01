cd2c06d0496087c4882265f63300a836bc95b161
jeff <jeffrey-31@hotmail.fr>
2025-10-01 09:52:17 +0200

    Update: Install dependencies swagger prisma class validator and class transformer

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Coucou Ingrid API')
+    .setDescription('Documentation de l’API Zafira')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // ignore les champs non déclarés dans les DTOs
+      forbidNonWhitelisted: true, // rejette si des champs inconnus sont envoyés
+      transform: true, // convertit les types (string → number, etc.)
+    }),
+  );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
+
 bootstrap();
