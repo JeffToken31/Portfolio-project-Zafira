c5e204346884f3119ecbe161b09d594ff0350d2f
mornac <imornac@gmail.com>
2025-10-15 16:07:01 +0200

    Add the Blog section and its components

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -3,12 +3,14 @@
 
 import HeroSection from '@/components/sections/HeroSection';
 import ActionSection from '@/components/sections/ActionsSection';
+import BlogSection from '@/components/sections/BlogSection';
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
       <ActionSection />
+      <BlogSection />
     </>
   );
 }
