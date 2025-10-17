be3cfc73007d1298bda627b6ce177ff6655616c2
jeff <jeffrey-31@hotmail.fr>
2025-10-17 15:39:05 +0200

    feat: page login for mobile version

---
+++ app/frontend/app/(auth)/login/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import LoginForm from '@/components/forms/LoginForm';
+
+export default function RegisterPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-bg p-6">
+      <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
+        <h1 className="text-2xl font-heading mb-4">Créer un compte</h1>
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
