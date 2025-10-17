e5d9a9a656e01915265b68bf699fa4d9982f0569
jeff <jeffrey-31@hotmail.fr>
2025-10-17 15:32:21 +0200

    Feat: update stategy JWT

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.use(cookieParser());
 
   app.useGlobalPipes(
     new ValidationPipe({
--- app/backend/src/modules/auth/infra/strategy/jwt.strategy.ts
+++ app/backend/src/modules/auth/infra/strategy/jwt.strategy.ts
@@ -1,12 +1,9 @@
 // src/common/strategies/jwt.strategy.ts
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import {
-  Strategy as JwtStrategyBase,
-  ExtractJwt,
-  StrategyOptions,
-} from 'passport-jwt';
+import { Strategy as JwtStrategyBase } from 'passport-jwt';
 import { AuthUser } from '../../interface/auth-user.interface';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(JwtStrategyBase) {
@@ -15,23 +12,41 @@ export class JwtStrategy extends PassportStrategy(JwtStrategyBase) {
     if (!secret) {
       throw new Error('JWT_SECRET not defined in environment');
     }
-    const options: StrategyOptions = {
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    super({
+      // keep JWT from cookie
+      jwtFromRequest: (req: Request): string | null => {
+        const token = (req.cookies as { [key: string]: string })?.[
+          'auth_token'
+        ];
+        if (typeof token === 'string') return token;
+        return null;
+      },
       ignoreExpiration: false,
       secretOrKey: secret,
-    };
-    super(options);
+    });
   }
 
-  validate(payload: {
-    sub: string;
-    email: string;
-    role: string;
-  }): Promise<AuthUser> {
-    return Promise.resolve({
-      id: payload.sub,
-      email: payload.email,
-      role: payload.role,
-    });
+  validate(payload: unknown): Promise<AuthUser> {
+    if (
+      typeof payload === 'object' &&
+      payload !== null &&
+      'sub' in payload &&
+      'email' in payload &&
+      'role' in payload
+    ) {
+      const p = payload as {
+        sub: string;
+        email: string;
+        role: string;
+        firstName?: string;
+      };
+      return Promise.resolve({
+        id: p.sub,
+        email: p.email,
+        role: p.role,
+        firstName: p.firstName ?? '', // fallback
+      });
+    }
+    throw new Error('Invalid JWT payload');
   }
 }
