646a38ad9007d9d95ee680edc094e9e468ee3809
jeff <jeffrey-31@hotmail.fr>
2025-10-19 09:55:29 +0200

    feat: réorganize pages dashboard for the next feature

---
+++ app/frontend/app/dashboard/beneficiary/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+
+export default function RegisterPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-bg p-6">
+      <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
+        <h1 className="text-2xl font-heading mb-4">Dashboard user</h1>
+      </div>
+    </div>
+  );
+}
